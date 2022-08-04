04f69062bc4355a1eeea5380021056e35e638c07
Øyvind Isaksen <oyvindisaks1@gmail.com>
2022-08-04 16:30:12 +0200

    chore: add get locations integration test

--- src/test/kotlin/no/oyvindis/kotlin_service/utils/ApiTestContext.kt
+++ src/test/kotlin/no/oyvindis/kotlin_service/utils/ApiTestContext.kt
@@ -1,7 +1,12 @@
 package no.oyvindis.kotlin_service.utils
 
 import org.slf4j.LoggerFactory
+import org.springframework.boot.test.util.TestPropertyValues
 import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.context.ApplicationContextInitializer
+import org.springframework.context.ConfigurableApplicationContext
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.containers.wait.strategy.Wait
 import java.io.IOException
 import java.net.HttpURLConnection
 import java.net.URL
@@ -11,13 +16,31 @@ abstract class ApiTestContext {
     @LocalServerPort
     var port: Int = 0
 
+    internal class Initializer : ApplicationContextInitializer<ConfigurableApplicationContext> {
+        override fun initialize(configurableApplicationContext: ConfigurableApplicationContext) {
+            TestPropertyValues.of(
+                "spring.data.mongodb.uri=mongodb://$MONGO_USER:$MONGO_PASSWORD@localhost:${mongoContainer.getMappedPort(MONGO_PORT)}/$MONGO_DB_NAME?authSource=admin&authMechanism=SCRAM-SHA-1"
+            ).applyTo(configurableApplicationContext.environment)
+        }
+    }
+
     companion object {
 
         private val logger = LoggerFactory.getLogger(ApiTestContext::class.java)
+        var mongoContainer: KGenericContainer
 
         init {
 
             startMockServer()
+
+            mongoContainer = KGenericContainer("mongo:latest")
+                .withEnv(MONGO_ENV_VALUES)
+                .withExposedPorts(MONGO_PORT)
+                .waitingFor(Wait.forListeningPort())
+
+            mongoContainer.start()
+
+            populate()
 
             try {
                 val con = URL("http://localhost:5000/ping").openConnection() as HttpURLConnection
@@ -36,3 +59,6 @@ abstract class ApiTestContext {
     }
 
 }
+
+// Hack needed because testcontainers use of generics confuses Kotlin
+class KGenericContainer(imageName: String) : GenericContainer<KGenericContainer>(imageName)
--- src/test/kotlin/no/oyvindis/kotlin_service/utils/TestUtils.kt
+++ src/test/kotlin/no/oyvindis/kotlin_service/utils/TestUtils.kt
@@ -1,5 +1,13 @@
 package no.oyvindis.kotlin_service.utils
 
+import com.mongodb.ConnectionString
+import com.mongodb.MongoClientSettings
+import com.mongodb.client.MongoClient
+import com.mongodb.client.MongoClients
+import no.oyvindis.kotlin_service.model.LocationDBO
+import no.oyvindis.kotlin_service.utils.ApiTestContext.Companion.mongoContainer
+import org.bson.codecs.configuration.CodecRegistries
+import org.bson.codecs.pojo.PojoCodecProvider
 import org.springframework.http.*
 import java.io.BufferedReader
 import java.net.HttpURLConnection
@@ -36,3 +44,27 @@ fun apiGet(port: Int, endpoint: String, acceptHeader: String?): Map<String,Any> 
 }
 
 private fun isOK(response: Int?): Boolean = HttpStatus.resolve(response ?: 0)?.is2xxSuccessful ?: false
+
+fun populate() {
+    val connectionString = ConnectionString("mongodb://${MONGO_USER}:${MONGO_PASSWORD}@localhost:${mongoContainer.getMappedPort(MONGO_PORT)}/$MONGO_DB_NAME?authSource=admin&authMechanism=SCRAM-SHA-1")
+    val pojoCodecRegistry = CodecRegistries.fromRegistries(
+        MongoClientSettings.getDefaultCodecRegistry(), CodecRegistries.fromProviders(
+            PojoCodecProvider.builder().automatic(true).build()))
+
+    val client: MongoClient = MongoClients.create(connectionString)
+    val mongoDatabase = client.getDatabase(MONGO_DB_NAME).withCodecRegistry(pojoCodecRegistry)
+
+    val locationCollection = mongoDatabase.getCollection("location")
+    locationCollection.deleteMany(org.bson.Document())
+    locationCollection.insertMany(locationDbPopulation())
+
+}
+
+private fun LocationDBO.mapBDO(): org.bson.Document =
+    org.bson.Document()
+        .append("_id", id)
+        .append("name", name)
+
+fun locationDbPopulation() = listOf(LOCATION_1000)
+    .map { it.mapBDO() }
+
