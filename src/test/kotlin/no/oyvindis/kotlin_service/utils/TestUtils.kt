4b00c461d218cfa16ed3a535991ed8bb56162ab5
Øyvind Isaksen <oyvindisaks1@gmail.com>
2022-08-05 11:25:53 +0200

    chore: add reading integration test

--- src/test/kotlin/no/oyvindis/kotlin_service/utils/TestUtils.kt
+++ src/test/kotlin/no/oyvindis/kotlin_service/utils/TestUtils.kt
@@ -5,13 +5,18 @@ import com.mongodb.MongoClientSettings
 import com.mongodb.client.MongoClient
 import com.mongodb.client.MongoClients
 import no.oyvindis.kotlin_service.model.LocationDBO
+import no.oyvindis.kotlin_service.model.ReadingDBO
 import no.oyvindis.kotlin_service.utils.ApiTestContext.Companion.mongoContainer
 import org.bson.codecs.configuration.CodecRegistries
 import org.bson.codecs.pojo.PojoCodecProvider
 import org.springframework.http.*
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
+import org.springframework.web.client.HttpClientErrorException
+import org.springframework.web.client.RestTemplate
 import java.io.BufferedReader
 import java.net.HttpURLConnection
 import java.net.URL
+import java.time.LocalDateTime
 
 
 fun apiGet(port: Int, endpoint: String, acceptHeader: String?): Map<String,Any> {
@@ -45,6 +50,47 @@ fun apiGet(port: Int, endpoint: String, acceptHeader: String?): Map<String,Any> 
 
 private fun isOK(response: Int?): Boolean = HttpStatus.resolve(response ?: 0)?.is2xxSuccessful ?: false
 
+fun authorizedRequest(
+    path: String,
+    port: Int,
+    body: String? = null,
+    token: String? = null,
+    httpMethod: HttpMethod,
+    accept: MediaType = MediaType.APPLICATION_JSON
+): Map<String, Any> {
+    val request = RestTemplate()
+    request.requestFactory = HttpComponentsClientHttpRequestFactory()
+    val url = "http://localhost:$port$path"
+    val headers = HttpHeaders()
+    headers.accept = listOf(accept)
+    token?.let { headers.setBearerAuth(it) }
+    headers.contentType = MediaType.APPLICATION_JSON
+    val entity: HttpEntity<String> = HttpEntity(body, headers)
+
+    return try {
+        val response = request.exchange(url, httpMethod, entity, String::class.java)
+        mapOf(
+            "body" to response.body,
+            "header" to response.headers.toString(),
+            "status" to response.statusCode.value()
+        )
+
+    } catch (e: HttpClientErrorException) {
+        mapOf(
+            "status" to e.rawStatusCode,
+            "header" to " ",
+            "body" to e.toString()
+        )
+    } catch (e: Exception) {
+        mapOf(
+            "status" to e.toString(),
+            "header" to " ",
+            "body" to " "
+        )
+    }
+
+}
+
 fun populate() {
     val connectionString = ConnectionString("mongodb://${MONGO_USER}:${MONGO_PASSWORD}@localhost:${mongoContainer.getMappedPort(MONGO_PORT)}/$MONGO_DB_NAME?authSource=admin&authMechanism=SCRAM-SHA-1")
     val pojoCodecRegistry = CodecRegistries.fromRegistries(
@@ -58,6 +104,9 @@ fun populate() {
     locationCollection.deleteMany(org.bson.Document())
     locationCollection.insertMany(locationDbPopulation())
 
+    val readingCollection = mongoDatabase.getCollection("reading")
+    readingCollection.deleteMany(org.bson.Document())
+    readingCollection.insertMany(readingDbPopulation())
 }
 
 private fun LocationDBO.mapBDO(): org.bson.Document =
@@ -66,5 +115,23 @@ private fun LocationDBO.mapBDO(): org.bson.Document =
         .append("name", name)
 
 fun locationDbPopulation() = listOf(LOCATION_1000)
+    .map { it.mapBDO() }
+
+private fun ReadingDBO.mapBDO(): org.bson.Document =
+    org.bson.Document()
+        .append("_id", id)
+        .append("location", location)
+        .append("localDateTime", LocalDateTime.now())
+        .append("battery", battery)
+        .append("co2", co2)
+        .append("humidity", humidity)
+        .append("pm1", pm1)
+        .append("pm25", pm25)
+        .append("pressure", pressure)
+        .append("temp", temp)
+        .append("time", time)
+        .append("voc", voc)
+
+fun readingDbPopulation() = listOf(READING_1000)
     .map { it.mapBDO() }
 
