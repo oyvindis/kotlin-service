7a145f4942ec5689880f0d893252fa396b758d23
Øyvind Isaksen <oyvindisaks1@gmail.com>
2022-09-09 14:55:27 +0200

    Revert "feat: add scheduled post reading from airthings"
    This reverts commit 0f6c90ff8a4d17b28c4d05e3a2be8590c17dbc95.

--- src/main/kotlin/no/oyvindis/kotlin_service/service/ReadingService.kt
+++ src/main/kotlin/no/oyvindis/kotlin_service/service/ReadingService.kt
@@ -7,23 +7,14 @@ import no.oyvindis.kotlin_service.repository.LocationDAO
 import no.oyvindis.kotlin_service.repository.ReadingDAO
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.scheduling.annotation.Scheduled
-import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction
 import org.springframework.stereotype.Service
-import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.bodyToMono
 
 private val logger: Logger = LoggerFactory.getLogger(ReadingService::class.java)
-
-class AirthingsResponse {
-    val data: Reading? = null
-}
 
 @Service
 class ReadingService(
     private val readingDAO: ReadingDAO,
-    private val locationDAO: LocationDAO,
-    private val webClient: WebClient
+    private val locationDAO: LocationDAO
 ) {
     private fun createLocationIfNotExists(id: String) {
         try {
@@ -32,7 +23,7 @@ class ReadingService(
                 locationDAO.insert(locationDocument)
             }
         } catch (ex: Exception) {
-            logger.error("insert location failed", ex)
+           logger.error("insert location failed", ex)
         }
     }
 
@@ -40,40 +31,16 @@ class ReadingService(
         createLocationIfNotExists(location)
 
         if (!locationDAO.existsById(location)) {
-            throw object : Exception("Location not found") {}
+            throw object: Exception("Location not found") {}
         }
         val newReading: ReadingDBO = reading.mapForCreation(location)
 
         return readingDAO
             .insert(newReading)
-            ?.let { it.toDTO(locationDAO.findById(it.location)) }
+            ?.let { it.toDTO(locationDAO.findById(it.location))}
     }
 
     fun getReadingsByLocation(locationId: String): List<Reading> = readingDAO.findReadingsByLocation(locationId)
-        .map { it.toDTO(locationDAO.findById(it.location)) }
-
-    @Scheduled(fixedRate = 21600000)
-    fun postReadingFromAirthings() {
-        logger.debug("postReadingFromAirthings")
-        val data = webClient
-            .get()
-            .uri("https://ext-api.airthings.com/v1/devices/2960009475/latest-samples")
-            .attributes(
-                ServerOAuth2AuthorizedClientExchangeFilterFunction
-                    .clientRegistrationId("airthings")
-            )
-            .retrieve()
-            .bodyToMono<AirthingsResponse>()
-        val responseStr = data.block()
-
-        val newReading = responseStr?.data?.mapForCreation("2960009475")
-        logger.debug(newReading.toString())
-
-        if (newReading !== null) {
-            readingDAO
-                .insert(newReading).let { it.toDTO(locationDAO.findById(it.location)) }
-        }
-
-    }
+        .map { it.toDTO(locationDAO.findById(it.location))}
 
 }
--- src/test/kotlin/no/oyvindis/kotlin_service/unit/ReadingService.kt
+++ src/test/kotlin/no/oyvindis/kotlin_service/unit/ReadingService.kt
@@ -11,15 +11,13 @@ import no.oyvindis.kotlin_service.utils.LOCATION_ID_1000
 import no.oyvindis.kotlin_service.utils.READING_0
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
-import org.springframework.web.reactive.function.client.WebClient
 import kotlin.test.assertTrue
 
 @Tag("unit")
 class ReadingService: ApiTestContext() {
     private val readingDAO: ReadingDAO = mock()
     private val locationDAO: LocationDAO = mock()
-    private val webClient: WebClient = mock()
-    private val readingService = ReadingService(readingDAO, locationDAO, webClient)
+    private val readingService = ReadingService(readingDAO, locationDAO)
 
     @Test
     fun `Get all readings by location id` () {
