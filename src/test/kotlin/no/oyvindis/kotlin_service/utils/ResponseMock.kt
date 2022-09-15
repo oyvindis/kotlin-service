3f0e503be57bd22a18503b3cbea5dff5603c03af
Øyvind Isaksen <oyvindisaks1@gmail.com>
2022-09-15 11:43:25 +0200

    feat: add security and extend integration tests with authentication

--- src/test/kotlin/no/oyvindis/kotlin_service/utils/ResponseMock.kt
+++ src/test/kotlin/no/oyvindis/kotlin_service/utils/ResponseMock.kt
@@ -2,6 +2,7 @@ package no.oyvindis.kotlin_service.utils
 
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock.*
+import no.oyvindis.kotlin_service.utils.jwk.JwkStore
 
 const val LOCAL_SERVER_PORT = 5000
 
@@ -11,7 +12,8 @@ fun startMockServer() {
     if(!mockserver.isRunning) {
         mockserver.stubFor(get(urlEqualTo("/ping"))
             .willReturn(aResponse().withStatus(200)))
-
+        mockserver.stubFor(get(urlEqualTo("/oauth2/default"))
+            .willReturn(okJson(JwkStore.get())))
         mockserver.start()
     }
 }
