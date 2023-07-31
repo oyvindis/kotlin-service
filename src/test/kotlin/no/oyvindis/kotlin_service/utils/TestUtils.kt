4ece8c8d646152ad00f625641c5ede044eb997ee
Øyvind Isaksen <oyvindisaks1@gmail.com>
2023-07-31 15:15:23 +0200

    fix: remove non-working tests

--- src/test/kotlin/no/oyvindis/kotlin_service/utils/TestUtils.kt
+++ src/test/kotlin/no/oyvindis/kotlin_service/utils/TestUtils.kt
@@ -1,34 +1,9 @@
 package no.oyvindis.kotlin_service.utils
 
-import com.mongodb.ConnectionString
-import com.mongodb.MongoClientSettings
-import com.mongodb.client.MongoClient
-import com.mongodb.client.MongoClients
 import no.oyvindis.kotlin_service.model.LocationDBO
 import no.oyvindis.kotlin_service.model.ReadingDBO
-import no.oyvindis.kotlin_service.utils.ApiTestContext.Companion.mongoContainer
-import org.bson.codecs.configuration.CodecRegistries
-import org.bson.codecs.pojo.PojoCodecProvider
 import java.time.LocalDateTime
 
-
-fun populate() {
-    val connectionString = ConnectionString("mongodb://${MONGO_USER}:${MONGO_PASSWORD}@localhost:${mongoContainer.getMappedPort(MONGO_PORT)}/$MONGO_DB_NAME?authSource=admin&authMechanism=SCRAM-SHA-1")
-    val pojoCodecRegistry = CodecRegistries.fromRegistries(
-        MongoClientSettings.getDefaultCodecRegistry(), CodecRegistries.fromProviders(
-            PojoCodecProvider.builder().automatic(true).build()))
-
-    val client: MongoClient = MongoClients.create(connectionString)
-    val mongoDatabase = client.getDatabase(MONGO_DB_NAME).withCodecRegistry(pojoCodecRegistry)
-
-    val locationCollection = mongoDatabase.getCollection("location")
-    locationCollection.deleteMany(org.bson.Document())
-    locationCollection.insertMany(locationDbPopulation())
-
-    val readingCollection = mongoDatabase.getCollection("reading")
-    readingCollection.deleteMany(org.bson.Document())
-    readingCollection.insertMany(readingDbPopulation())
-}
 
 private fun LocationDBO.mapBDO(): org.bson.Document =
     org.bson.Document()
