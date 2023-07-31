231321cf10d5c06afa4e8a0f9009c97687219c8d
Øyvind Isaksen <oyvindisaks1@gmail.com>
2023-07-31 17:10:30 +0200

    chore: simplify pom, update dependencies, Dockerfile and workflow

--- src/main/kotlin/no/oyvindis/kotlin_service/config/SlackChannel.kt
+++ src/main/kotlin/no/oyvindis/kotlin_service/config/SlackChannel.kt
@@ -3,7 +3,6 @@ package no.oyvindis.kotlin_service.config
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
 
-@ConstructorBinding
 @ConfigurationProperties("slack.channel")
 data class SlackChannel(
     val id: String
